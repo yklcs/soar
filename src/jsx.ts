3e09596d6ff7eddbdb3911564369be1c21ed301f
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-02-05 15:34:51 +0900

    scoped css styles

--- src/jsx.ts
+++ src/jsx.ts
@@ -1,11 +1,8 @@
-// import { browserslistToTargets, transform as transformCss } from "lightningcss"
-// import browserslist from "browserslist"
-import { parseHTML } from "linkedom"
-
 interface VNode {
 	type: JSX.ElementType
 	children?: JSX.Children
 	props: JSX.Props
+	scope?: string
 	style?: string
 	styled: (style: string) => VNode
 }
@@ -13,105 +10,16 @@ interface VNode {
 const jsx = (
 	type: JSX.ElementType,
 	{ children, ...props }: JSX.Props,
-): VNode => {
-	const vnode: VNode = {
-		type,
-		children,
-		props,
-		style: undefined,
-		styled: (style: string) => {
-			vnode.style = style
-			return vnode
-		},
-	}
-
-	return vnode
-}
-
-const renderToString = async (root: VNode): Promise<string> => {
-	const { document } = parseHTML(
-		"<!DOCTYPE html><html><head></head><body></body></html>",
-	)
-	await render(root, document)
-	return document.toString()
-}
-
-const digest = async (message: string) => {
-	const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
-	const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8) // hash the message
-	const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
-	const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("") // convert bytes to hex string
-	return hashHex
-}
-
-const render = async (root: VNode, document: Document): Promise<undefined> => {
-	let styles: Record<string, string> = {}
-
-	const _render = async (
-		children: JSX.Children,
-		parent: Node,
-		scope?: string,
-	) => {
-		if (Array.isArray(children)) {
-			for (const child of children) {
-				_render(child, parent, scope)
-			}
-			return
-		}
-
-		if (typeof children === "string") {
-			const text = document.createTextNode(children)
-			parent.appendChild(text)
-			return
-		}
-
-		let newscope: string | undefined = undefined
-		if (children.style !== undefined) {
-			newscope = (await digest(children.style)).slice(0, 8)
-			styles[newscope] = children.style
-		}
-
-		if (typeof children.type === "string") {
-			let el: HTMLElement
-			if (
-				children.type === "html" ||
-				children.type === "head" ||
-				children.type === "body"
-			) {
-				// biome-ignore lint/style/noNonNullAssertion: document will have these elements
-				el = document.querySelector(children.type)!
-			} else {
-				el = document.createElement(children.type)
-				parent.appendChild(el)
-			}
-			el.setAttribute("scope", newscope ?? scope ?? "")
-			children.children && _render(children.children, el, scope ?? newscope)
-		} else if (typeof children.type === "function") {
-			const inner = await children.type({
-				...children.props,
-				children: children.children,
-			})
-			_render(inner, parent, scope ?? newscope)
-		} else {
-			console.log(children)
-		}
-	}
-
-	await _render(root, document)
-
-	// const scopeCss = ([scope, style]: [string, string]) =>
-	// 	`["scope"=${scope}] {${style}}`
-	// const style = Object.entries(styles).map(scopeCss).join("\n")
-
-	// const targets = browserslistToTargets(browserslist(">= 0.25%"))
-	// const { code, map } = transformCss({
-	// 	filename: "style.css",
-	// 	code: Buffer.from(style),
-	// 	targets,
-	// })
-
-	// console.log(code)
-}
+): VNode => ({
+	type,
+	children,
+	props,
+	style: undefined,
+	styled(style: string) {
+		this.style = style
+		return this
+	},
+})
 
 const Fragment = ({ children }: JSX.Props) => children
 
@@ -317,4 +225,4 @@ declare namespace JSX {
 	}
 }
 
-export { render, renderToString, jsx, jsx as jsxs, Fragment, JSX }
+export { VNode, jsx, jsx as jsxs, Fragment, JSX }
---
+++ src/render.ts
@@ -0,0 +1,150 @@
+import {
+	SelectorComponent,
+	browserslistToTargets,
+	transform as transformCss,
+} from "lightningcss"
+import browserslist from "browserslist"
+import { parseHTML } from "linkedom"
+import { JSX, VNode } from "jsx.js"
+
+const renderToString = async (root: VNode): Promise<string> => {
+	const { document } = parseHTML(
+		"<!DOCTYPE html><html><head></head><body></body></html>",
+	)
+	await render(root, document)
+	return document.toString()
+}
+
+const digest = async (message: string) => {
+	const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
+	const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8) // hash the message
+	const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
+	const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("") // convert bytes to hex string
+	return hashHex
+}
+
+type RenderedNode = Node | RenderedNode[]
+
+const applyScope = async (root: JSX.Children, scope: string) => {
+	if (typeof root === "string") {
+		return
+	}
+	if (Array.isArray(root)) {
+		for (const child of root) {
+			await applyScope(child, scope)
+		}
+	} else {
+		if (root.scope === undefined) {
+			root.scope = scope
+		}
+		root.children && (await applyScope(root.children, scope))
+	}
+}
+
+const render = async (root: VNode, document: Document): Promise<undefined> => {
+	const styles: Record<string, string> = {}
+
+	const _render = async (
+		node: JSX.Children,
+		parent: Node,
+	): Promise<RenderedNode> => {
+		if (Array.isArray(node)) {
+			return Promise.all(node.map((child) => _render(child, parent)))
+		}
+
+		if (typeof node === "string") {
+			const text = document.createTextNode(node)
+			parent.appendChild(text)
+			return text
+		}
+
+		// new scopes are created for styled nodes or functional elements
+		if (node.style !== undefined || typeof node.type === "function") {
+			const newscope = (await digest(node.style ?? node.type.toString())).slice(
+				0,
+				8,
+			)
+			styles[newscope] = node.style ?? ""
+			await applyScope(node, newscope)
+			node.scope = newscope
+		}
+
+		if (typeof node.type === "string") {
+			let el: HTMLElement
+			if (
+				node.type === "html" ||
+				node.type === "head" ||
+				node.type === "body"
+			) {
+				// biome-ignore lint/style/noNonNullAssertion: document will have these elements
+				el = document.querySelector(node.type)!
+			} else {
+				el = document.createElement(node.type)
+				parent.appendChild(el)
+			}
+
+			for (const [key, val] of Object.entries(node.props)) {
+				el.setAttribute(key, val)
+			}
+
+			// set scope only if node is in a scope
+			node.scope && el.setAttribute("scope", node.scope)
+
+			node.children && (await _render(node.children, el))
+
+			return el
+		}
+
+		if (typeof node.type === "function") {
+			const inner = await node.type({
+				...node.props,
+				children: node.children,
+			})
+
+			// render children with new scope or old scope
+			return await _render(inner, parent)
+		}
+
+		// fallback
+		const text = document.createTextNode(node.toString())
+		parent.appendChild(text)
+		return text
+	}
+
+	await _render(root, document)
+
+	const scopeCss = ([scope, style]: [string, string]) =>
+		`[scope="${scope}"] {${style}}`
+	const rawCss = Object.entries(styles).map(scopeCss).join("\n")
+
+	const targets = browserslistToTargets(browserslist(">= 0.25%"))
+
+	const css: string[] = []
+	for (const [scope, rawCss] of Object.entries(styles)) {
+		const { code, map } = transformCss({
+			filename: "style.css",
+			code: Buffer.from(rawCss),
+			targets,
+			visitor: {
+				Selector(selector) {
+					const scopeSelector: SelectorComponent = {
+						type: "attribute",
+						name: "scope",
+						operation: {
+							operator: "equal",
+							value: scope,
+						},
+					}
+					return [selector[0], scopeSelector, ...selector.slice(1)]
+				},
+			},
+		})
+		css.push(code.toString())
+	}
+
+	const style = document.createElement("style")
+	style.textContent = css.join("\n")
+	document.head.appendChild(style)
+}
+
+export { render, renderToString }
